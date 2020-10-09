3de508218adde029f5db8f814c7027b165724e93
Luis Antonio Parrado <luisprmat@gmail.com>
2020-10-09 00:35:57 -0500

    Install and set PHPUnit

---
+++ tests/FirstTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+    /** @test */
+    function first_test()
+    {
+        $this->assertTrue(True);
+    }
+}
